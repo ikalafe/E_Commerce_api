b8277713f55dc3caf8bb955202e16960872ce521
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-15 15:00:56 +0330

    fix(order): implement changeOrderStatus and deleteOrder methods in orders controller

--- controllers/admin/orders.js
+++ controllers/admin/orders.js
@@ -1,5 +1,5 @@
 const { Order } = require("../../models/order");
-const { Product } = require("../../models/product");
+const { OrderItem } = require("../../models/order_Item");
 
 exports.getOrders = async (req, res) => {
   try {
@@ -24,17 +24,51 @@ exports.getOrders = async (req, res) => {
   }
 };
 
-exports.getOrdersCount = async (req, res) => {
+exports.getOrdersCount = async (_, res) => {
   try {
     const count = await Order.countDocuments();
     if (!count)
       return res.status(500).json({ message: "خطا در شمارش سفارش‌ها رخ داد" });
-    
-    
+    return res.json({ count });
   } catch (error) {
     console.error("Error: ", error);
     return res.status(500).json({ type: error.name, message: error.message });
   }
 };
 
-exports.changeOrderStatus = async (req, res) => {};
+exports.changeOrderStatus = async (req, res) => {
+  try {
+    const orderId = req.params.id;
+    const newStatus = req.body.status;
+
+    const order = await Order.findById(orderId);
+    if (!order) return res.status(404).json({ message: "سفارشی یافت نشد." });
+
+    if (!order.statusHistory.includes(order.status)) {
+      order.statusHistory.push(order.status);
+    }
+    order.status = newStatus;
+    order = await order.save();
+    return res.json(order);
+  } catch (error) {
+    console.error("Error: ", error);
+    return res.status(500).json({ type: error.name, message: error.message });
+  }
+};
+
+exports.deleteOrder = async (req, res) => {
+  try {
+    const orderId = req.params.id;
+    const order = await Order.findByIdAndDelete(orderId);
+    if (!order) return res.status(404).json({ message: "سفارشی یافت نشد." });
+
+    for (const orderItemId of order.orderItems) {
+      await OrderItem.findByIdAndDelete(orderItemId);
+    }
+
+    return res.status(204).end();
+  } catch (error) {
+    console.error("Error: ", error);
+    return res.status(500).json({ type: error.name, message: error.message });
+  }
+};
