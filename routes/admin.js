ac7dbf54b257fc11a0520cdfc2bbe3cb0e17b114
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-15 15:01:05 +0330

    feat(admin): implement product management routes in admin controller

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const usersController = require("../controllers/admin/users");
 const categoriesController = require("../controllers/admin/categories");
 const ordersController = require("../controllers/admin/orders");
+const productController = require("../controllers/admin/products")
 
 // USERS
 router.get("/users/count", usersController.getUserCount);
@@ -15,15 +16,17 @@ router.put("/categories/:id", categoriesController.editCategory);
 router.delete("/categories/:id", categoriesController.deleteCategory);
 
 // PRODUCTS
-// router.get("/products/count", adminController.getProductsCount);
-// router.post("/products", adminController.addProduct);
-// router.put("/products/:id", adminController.editProduct);
-// router.delete("/products/:id/images", adminController.deleteProductImages);
-// router.delete("/products/:id", adminController.deleteProduct);
+router.get("/products/count", productController.getProductsCount);
+router.get("/products", productController.getProducts);
+router.post("/products", productController.addProduct);
+router.put("/products/:id", productController.editProduct);
+router.delete("/products/:id/images", productController.deleteProductImages);
+router.delete("/products/:id", productController.deleteProduct);
 
 // ORDERS
 router.get("/orders", ordersController.getOrders);
 router.get("orders/count", ordersController.getOrdersCount);
 router.put("orders/:id", ordersController.changeOrderStatus);
+router.delete("orders/:id", ordersController.deleteOrder);
 
 module.exports = router;
